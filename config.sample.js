f33d515756c43ab1923cbb135baa9d4eccaba28e
kkdai <evanslin@gmail.com>
2025-10-10 16:35:50 +0800

    chore(config): remove sample configuration file

--- config.sample.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    "vcId": "YOUR_VC_ID",
-    "vcCid": "YOUR_VC_CID",
-    "apiKey": "YOUR_ISSUER_API_KEY",
-    "verifier_ref": "YOUR_VERIFIER_REF",
-    "verifier_accessToken": "YOUR_VERIFIER_ACCESS_TOKEN"
-}
